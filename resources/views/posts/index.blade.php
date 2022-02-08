f82d677280bc2d7e17b19487c993536f484cb627
nonoco <namakenono@gmail.com>
2022-02-08 23:20:03 +0900

    index.blade.php変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,34 +6,33 @@
 <div class="post-container">
   {!! Form::open(['url' => 'post/create']) !!}
   <div class="post-wrapper">
-    <img src="">
-    <!-- $images -->
+    <!--イメージを出せない-->
+    <img>{{ $userimage }}</img>
     <div class="post-item">
       {{ Form::input('text','newPosts',null,['class' => 'form-control','placeholder'=>'何をつぶやこうか…？'])}}
     </div>
-    <input type="image" name="submit" src="images/post.png" alt="送信">
+    <input type="image" class="submit" name="submit" src="images/post.png" alt="送信">
     {!! Form::close() !!}
   </div>
   <!--タイムライン-->
   <div class="timeline">
-    <table class="timeline-container">
+    <div class="timeline-container">
       @foreach ($post as $post)
-      <tr>
-        <div class="timeline-wrapper">
-          <img src="images/dawn.pug">
-          <!-- $images -->
-          <div class="timeline-post">
-            <div class="timeline-name">
-              <tb>{{ $post->username }}</tb>
-              <tb>{{ $post->created_at }}</tb>
-            </div>
-            <tb>{{ $post->posts }}</tb>
-          </div>
+      <div class="timeline-wrapper">
+        <!--イメージを出せない-->
+        <div class="timeline-image">
+          <img>{{ $post->images }}
         </div>
-      </tr>
+        <div>
+          <div class="timeline-name">
+            <p class="username">{{ $post->username }}</p>
+            <p class="created-at">{{ $post->created_at }}</p>
+          </div>
+          <p class="timeline-post">{{ $post->posts }}</p>
+        </div>
+      </div>
       @endforeach
-    </table>
+    </div>
   </div>
-
 </div>
 @endsection
