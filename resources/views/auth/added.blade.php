f72eb05812ce23295c19793287d1c9ae8caab5d6
nonoco <namakenono@gmail.com>
2022-02-23 18:09:16 +0900

    addedのcss

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -2,13 +2,16 @@
 
 @section('content')
 
-<div id="clear">
-  <p>{{ $list->username }}さん、</p>
-  <p>ようこそ！DAWNSNSへ！</p>
-  <p>ユーザー登録が完了しました。</p>
-  <p>さっそく、ログインをしてみましょう。</p>
-
-  <p class="btn"><a href="/login">ログイン画面へ</a></p>
+<div class="form">
+  <div class="added">
+    <p>{{ $list->username }}さん、</p>
+    <p>ようこそ！DAWNSNSへ！</p>
+  </div>
+  <div class="added-text">
+    <p>ユーザー登録が完了しました。</p>
+    <p>さっそく、ログインをしてみましょう。</p>
+  </div>
+  <p class="login-submit"><a href="/login" class="login-btn">ログイン画面へ</a></p>
 </div>
 
 @endsection
