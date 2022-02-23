39addaaaf18bdadb32a1b3e91d4893908be806a1
nonoco <namakenono@gmail.com>
2022-02-23 16:39:38 +0900

    ログイン画面のcss

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,27 @@
 @section('content')
 
 {!! Form::open() !!}
-
-<p>DAWNSNSへようこそ</p>
-
-{{ Form::label('MAilAddress') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
-{{ Form::label('password') }}
-{{ Form::password('password',['class' => 'input']) }}
-
-{{ Form::submit('ログイン') }}
-
-<p><a href="/register">新規ユーザーの方はこちら</a></p>
-
+<div class="form">
+  <p class="form-title">DAWNSNSへようこそ</p>
+  <div>
+    <div class="label">
+      {{ Form::label('MAilAddress') }}
+    </div>
+    <div>
+      {{ Form::text('mail',null,['class' => 'input']) }}
+    </div>
+    <div class="label">
+      {{ Form::label('password') }}
+    </div>
+    <div>
+      {{ Form::password('password',['class' => 'input']) }}
+    </div>
+    <div class="btn">
+      {{ Form::submit('ログイン',['class'=> 'submit']) }}
+    </div>
+  </div>
+  <p><a href="/register">新規ユーザーの方はこちら</a></p>
+</div>
 {!! Form::close() !!}
 
 @endsection
