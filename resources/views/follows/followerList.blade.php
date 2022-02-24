df42c4e357632115dfbef118adf8880f7836bf32
nonoco <namakenono@gmail.com>
2022-02-24 11:27:15 +0900

    followlist followerlistのcss設定

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 <!--フォローリスト-->
-<div class="">
-  @foreach($follower_image as $follower_image)
-  <div class="">
+<div class="follow-list">
+  <h2>Follower list</h2>
+  <div class="follow-icon">
+    @foreach($follower_image as $follower_image)
     <a href="/post/{{ $follower_image->id }}/profile">
       <img src="{{asset('images/'.$follower_image->images)}}">
     </a>
+    @endforeach
   </div>
-  @endforeach
 </div>
 <!--タイムライン-->
 <div class="timeline">
@@ -21,29 +22,27 @@
           <img src="{{asset('images/'.$post->images)}}">
         </a>
       </div>
-      <div>
+      <div class="timeline-item">
         <div class="timeline-name">
           <p class="username">{{ $post->username }}</p>
           <p class="created-at">{{ $post->created_at }}</p>
         </div>
         <p class="timeline-post">{{ $post->posts }}</p>
-      </div>
-      @if($username === $post->username)
-      <div class="">
-        <button type="button">
-          <a href="/post/{{ $post->id }}/update">
-            <img src="images/edit.png">
-          </a>
-        </button>
-      </div>
-      <div class="">
-        <button type="button">
+        @if($username === $post->username)
+        <div class="icon">
+          <div class="update-icon">
+            <a href="/post/{{ $post->id }}/update">
+              <img src="images/edit.png">
+            </a>
+          </div>
+        </div>
+        <div class="delete-icon">
           <a href="/post/{{ $post->id }}/delete" onclick="return confirm('こちらの投稿を削除してもよろしいでしょうか？')">
             <img src="images/trash_h.png">
           </a>
-        </button>
+        </div>
+        @endif
       </div>
-      @endif
     </div>
     @endforeach
   </div>
